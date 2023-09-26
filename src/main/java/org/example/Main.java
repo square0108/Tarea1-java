09333ab5e4d1479524411453cc294eb66a9a5eb3
square <78096857+square0108@users.noreply.github.com>
2023-09-26 11:26:05 -0300

    Revert "testing branch commit/push"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Yo soy el verdadero Mike Tyson");
+        System.out.println("Yo soy el verdadero Guillermo Oliva");
 
     }
 }
