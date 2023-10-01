bcf5f1eb6f7aa2b8bd4230a6144ee05c0df05ee6
square0108 <martinmariollanos@gmail.com>
2023-10-01 05:13:48 -0300

    implementacion de algunos arraylist (especificamente, en Direccion), avance leve en OrdenCompra, hay que fijarse en las cardinalidades del UML presentado en el ppt del profesor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,18 @@
 package org.example;
 import java.util.Date;
+import java.util.ArrayList;
 // nota: Teams del ayudante: Emilio Ramos Montesino.
 
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Yo soy el verdadero Martin Llanos");
+        Direccion d1 = new Direccion("Arboledas Verdes 420");
+        Cliente cordero = new Cliente("Pepe","21.165.368-K",d1);
+        Cliente llama = new Cliente("Seth","22.22.3.4.5",d1);
+        System.out.println(d1.toString());
+        System.out.println(cordero.toString());
+        System.out.println(llama.toString());
 
     }
 }
@@ -19,6 +26,7 @@ class Cliente {
         this.nombre = n;
         this.rut = r;
         this.dirCliente = d;
+        dirCliente.addCliente(this);
         /* Si varios clientes tienen la misma direccion,
          * entonces sus propiedades dirCliente tienen la misma referencia
          * (no son instancias nuevas de Direccion)*/
@@ -35,7 +43,7 @@ class Cliente {
         /*Aqui falta el toString de dircliente?? -> this.dirCliente.toString() ¿?
 
         // si. Me agile*/
-        return ("Nombre: " + this.getNombre() + ", RUT: " + this.getRut() + this.dirCliente.toString());
+        return ("Nombre: " + this.getNombre() + ", RUT: " + this.getRut() + ", " + this.dirCliente.toString());
     }
 }
 
@@ -44,13 +52,43 @@ class Direccion {
     /* ¿Será necesario que apunten a la misma referencia?
     /* La respuesta es Si, lo he confirmado con el profesor Geoffrey. ¡Muy bien hecho!*/
     private String direccion = null;
+    private ArrayList<Cliente> arrayClientes;
 
-    /*Metodos getter y setter*/
-    public Direccion() {}
+    /* *Metodos getter y setter**/
+
+    public Direccion(String d) {
+        this.direccion = d;
+        arrayClientes = new ArrayList<>();
+    }
     public String getDir() {return direccion;}
     public void setDir(String inputString) {this.direccion = inputString;}
-    /**/
-    public String toString() {return ("Direccion: " + this.direccion);}
+
+    /* **************************/
+
+    public String toString() {
+        return ("Direccion: " + this.direccion );
+    }
+
+    /*Puede que esto sea innecessario, pero me guie por el PPT que describe las Cardinalidades de UML, asi que Direccion
+    * ahora tiene un ArrayList con todos los clientes que poseen esta direccion. */
+
+    @Deprecated
+    public String stringDeClientes(ArrayList<Cliente> ar) {
+        /*Esta funcion es solo para concatenar los nombres de los clientes con la misma direccion, dentro de toString().
+        * lo pense un poco mas, y quizas no tiene sentido poner esto en toString(). Dejare la funcion por mientras*/
+        if (arrayClientes.size() == 0) return null;
+        else {
+            StringBuilder s = new StringBuilder();
+            for (int i = 0; i < ar.size(); i++) {
+                s.append(ar.get(i).getNombre());
+                if (i < ar.size()-1) s.append(", ");
+            }
+            return s.toString();
+        }
+    }
+    public void addCliente(Cliente nuevoCliente) {
+        arrayClientes.add(nuevoCliente);
+    }
 
 }
 
@@ -59,7 +97,7 @@ class DocTributario {
     private String numero;
     private String rut;
     private Date fecha;
-    private Direccion dir;
+    private Direccion dirDoc;
     /*En el enunciado del programa dice que el documento tributario debe tener una dirección, esta no aparece
     en el diagrama UML. ¿Se habra estara malo el UML o tengo mala comprensión lectora?*/
 
@@ -70,7 +108,7 @@ class DocTributario {
         this.numero = n;
         this.rut = r;
         this.fecha = f;
-        this.dir = d;
+        this.dirDoc = d;
     }
     /*Metodos getter y setter de DocTributario*/
     public void setNumero(String num){this.numero = num;}
@@ -79,10 +117,10 @@ class DocTributario {
     public String getRut(){return this.rut;}
     public void setFecha(Date fech){this.fecha = fech;}
     public Date getFecha(){return this.fecha;}
-    public void setDir(Direccion drc){this.dir = drc;}
-    public Direccion getDir(){return this.dir;}
+    public void setDir(Direccion drc){this.dirDoc = drc;}
+    public Direccion getDir(){return this.dirDoc;}
     public String toString(){
-        return ("Numero: " + this.numero + ", RUT: " + this.rut + ", Fecha: " + this.fecha.toString() + ", " + this.dir.toString());
+        return ("Numero: " + this.numero + ", RUT: " + this.rut + ", Fecha: " + this.fecha.toString() + ", " + this.dirDoc.toString());
     }
 
 }
@@ -113,11 +151,15 @@ class Pago {
         this.monto = m;
         this.fecha = f;
     }
-    /*Metodos getter y setter de Pago*/
+    /* *Metodos getter y setter**/
+
     public void setFecha(Date fech){this.fecha = fech;}
     public Date getFecha(){return this.fecha;}
     public void setMonto(float m){this.monto = m;}
     public float getMonto(){return this.monto;}
+
+    /* **************************/
+
     public String toString(){
         return ("Pago. Monto: " + this.getMonto() + ", Fecha: " + this.getFecha().toString());
     }
@@ -170,7 +212,32 @@ class Tarjeta extends Pago {
     }
 }
 
-class OrdenCompra {}
+class OrdenCompra {
+    // Tengo varias confusiones aqui. Se supone que el documento tributario asociado a una orden de compra se emite al mismo tiempo (misma fecha entre ambos), y es solo uno por cada orden?
+    // Una agregacion tiene permitido crear objetos con new?
+    private Date fecha;
+    private String estado;
+    private Cliente comprador;
+    private DocTributario documento;
+    private ArrayList<Pago> arrayPagos;
+    public OrdenCompra(Date f, Cliente c){
+        this.fecha = f;
+        this.comprador = c;
+        this.documento = new DocTributario(comprador.getNombre(),comprador.getRut(),this.fecha,comprador.getDirCliente());
+        // No tengo claro que funcion cumple Estado en el problema.
+        this.estado = null;
+        this.arrayPagos = new ArrayList<>();
+    }
+
+    /* **Metodos getter y setter**/
+
+    /* ***************************/
+
+    public void addPago(Pago nuevoPago) {
+        arrayPagos.add(nuevoPago);
+    }
+
+}
 
 class DetalleOrden {}
 
